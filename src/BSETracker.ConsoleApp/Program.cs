f47915b4fc5cb38013d3f94d8ca67baab0dca70b
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-07-26 19:18:39 +0530

    Fetch the announcements page

--- src/BSETracker.ConsoleApp/Program.cs
+++ src/BSETracker.ConsoleApp/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BSETracker.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string url = "https://www.bseindia.com/corporates/ann.aspx?curpg=1&annflag=1&dt=&dur=A&dtto=&cat=&scrip=541154&anntype=C";
+            HttpClient client = new HttpClient();
+            Console.WriteLine(await client.GetStringAsync(url));
         }
     }
 }
