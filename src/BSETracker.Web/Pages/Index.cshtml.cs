a94141f6481b5fe209b363ca3c2613d88fd25252
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-27 00:03:56 +0530

    Handle conditions where the user has already registered

--- src/BSETracker.Web/Pages/Index.cshtml.cs
+++ src/BSETracker.Web/Pages/Index.cshtml.cs
@@ -3,13 +3,15 @@ using System.Threading.Tasks;
 using BSETracker.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace BSETracker.Web.Pages
 {
     public class IndexModel : PageModel
     {
-
         public bool? Success { get; set; }
+        public string Error { get; set; }
         private readonly AppDbContext db;
         public IndexModel(AppDbContext db) 
         {
@@ -26,12 +28,27 @@ namespace BSETracker.Web.Pages
 
         public async Task<PageResult> OnPostAsync()
         {
+            VM.Email = VM.Email.Trim().ToLowerInvariant();
             if(!ModelState.IsValid) {
                 return Page();
             }
 
-            var result = await db.Registrations.AddAsync(VM.ToRegistrationDM());
-            Success = await db.SaveChangesAsync() > 0;
+            try{
+                var result = await db.Registrations.AddAsync(VM.ToRegistrationDM());
+                Success = await db.SaveChangesAsync() > 0;
+            }
+            catch(DbUpdateException ex)
+            {
+                Success = false;
+                if(ex.InnerException is PostgresException)
+                {
+                    var inner = ex.InnerException as PostgresException;
+                    if(inner.SqlState == "23505")
+                    {
+                        Error = "You're already registered 😎";
+                    }
+                }
+            }
             return Page();
         }
 
